a082adb480176306ec8ff716034407048ec4bd99
Christoffer Olsen <chris70944@gmail.com>
2024-03-13 23:20:35 +0100

    Done

--- Øvelse 3.8/Program.cs
+++ Øvelse 3.8/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hvor varmt er det idag?");
+int grader = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"I morgen bliver det {(grader + 2)} grader");
+Console.Clear();
